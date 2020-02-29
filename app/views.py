8f4671e11bd710575db73e8868c699e364ac3eaa
Pascal <stehling.pascal@gmail.com>
2020-02-29 11:18:27 +0100

    Watchtower Private Docker Test

--- app/views.py
+++ app/views.py
@@ -8,6 +8,6 @@ def index():
     app_name = os.getenv("APP_NAME")
 
     if app_name:
-        return f"Hello from {app_name} running in a Docker container behind Nginx! This is some new text to try Watchtower."
+        return f"Hello from {app_name} running in a Docker container behind Nginx! This is some new text to try Watchtower, now with a private Repo"
 
     return "Hello from Flask"
